cc5378e9e964b2e53209dc35d449353e86acf974
Kam <santiago.carrilloc@ingenieria.unam.edu>
2023-04-25 13:47:36 -0700

    Se modificó la barra de navegacion

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,15 +1,12 @@
 <template>
     <nav class="flex items-center justify-between flex-wrap bg-red-900 p-6">
-        <div class="flex items-center flex-shrink-0 text-white mr-6 font-semibold text-xl tracking-tight">
-          Kam Carrillo
-        </div>
         <div class="block lg:hidden">
           <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
           </button>
         </div>
         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-          <router-link to="/" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 font-semibold px-10 py-1">
-            Inicio
+          <router-link to="/" class="flex items-center flex-shrink-0 text-white mr-6 font-semibold text-xl tracking-tight">
+            Kam Carrillo
           </router-link>
           <router-link to="/blog" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4 font-semibold px-10 py-1">
             Blog
